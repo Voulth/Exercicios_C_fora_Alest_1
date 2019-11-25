84b573d84595762a6d5fc0e3e47ff498e4575274
Voulth <mateusp2oliveira@hotmail.com>
2019-11-25 20:25:28 +0000

    Estudo

---
+++ stru.c
@@ -0,0 +1,17 @@
+//Fazer um programa que pede os dados da file dados.txt usando struct
+
+#include <stdio.h>
+
+struct pessoa{
+char nome[20];
+int idade;
+float altura;
+float peso;
+char cor_Fav[20];
+};
+
+void main(){
+
+
+
+}
