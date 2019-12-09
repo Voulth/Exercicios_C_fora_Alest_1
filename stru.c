12718f223922a9e7b4d84feb00946cf6c3d0bdf4
Voulth <mateusp2oliveira@hotmail.com>
2019-12-09 15:02:32 +0000

    Resolver Problema Fgets pegar tamanho certo, tentar usar while e fgets e malloc

--- stru.c
+++ stru.c
@@ -10,8 +10,31 @@ float peso;
 char cor_Fav[20];
 };
 
+void pegaDados(FILE *F,struct pessoa *p){
+while(fgets(p->nome,1,F)!=0){
+    fgets(p->nome,1,F);
+}
+fscanf(F,"%d",&p->idade);
+fscanf(F,"%f",&p->altura);
+fscanf(F,"%f",&p->peso);
+fgets(p->cor_Fav,7, F);
+}
+
+void imprimeDados(struct pessoa *p){
+printf("%s\n",p->nome);
+printf("%d\n",p->idade);
+printf("%f\n",p->altura);
+printf("%f\n",p->peso);
+printf("%s\n",p->cor_Fav);
+}
+
 void main(){
 
+struct pessoa a;
+FILE *Ar=fopen("dados.txt", "r");
+
+pegaDados(Ar,&a);
+imprimeDados(&a);
 
 
 }
